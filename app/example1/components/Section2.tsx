a32c0d83d9c02d9b6929f8da9fdcc2cdd6bb224a
Park JunYong <dyden119@gmail.com>
2024-06-14 18:39:40 +0900

    chore: 예제1번 정리중

--- app/example1/components/Section2.tsx
+++ app/example1/components/Section2.tsx
@@ -1,7 +1,27 @@
+import Phone from "@/components/Phone";
+
 export default function Section2() {
   return (
-    <div className="py-32 max-w-[1280px] mx-auto">
-      <div className="text-white">gd</div>
+    <div className="py-32 ">
+      <div className="text-white max-w-[1280px] mx-auto">
+        <div className="max-w-3xl">
+          <h2 className="text-3xl font-medium tracking-tight text-white">
+            Every feature you need to win. Try it for yourself.
+          </h2>
+          <p className="mt-2 text-lg text-gray-400">
+            Pocket was built for investors like you who play by their own rules
+            and aren’t going to let SEC regulations get in the way of their
+            dreams. If other investing tools are afraid to build it, Pocket has
+            it.
+          </p>
+        </div>
+        <div>
+          <Phone />
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 }
